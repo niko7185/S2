db761e90829527abe99d8d536b14b1e2c588c254
Nikolaj Schmidt Christoffersen <niko7185@edu.campusvejle.dk>
2020-02-26 09:37:59 +0100

    Add project files.

---
+++ S2.Oop2/ContanctInformation.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace S2.Oop2
+{
+    public class ContanctInformation
+    {
+
+        private string mail;
+        private string phone;
+
+        public string Mail
+        {
+            get => mail;
+
+            set
+            {
+
+                if(string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentNullException(nameof(mail), "Mail cannot be empty");
+                }
+
+                mail = value;
+
+            }
+        }
+
+        public string Phone
+        {
+            get => phone;
+
+            set
+            {
+
+                if(string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentNullException(nameof(phone), "Phone number cannot be empty");
+                }
+
+                phone = value;
+            }
+        }
+
+        public ContanctInformation(string mail, string phone)
+        {
+
+            Mail = mail;
+
+            Phone = phone;
+
+        }
+
+
+    }
+}
---
+++ S2.Oop2/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace S2.Oop2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+
+        }
+    }
+}
